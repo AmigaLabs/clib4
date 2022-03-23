bcdeed35bc44f7266508d9734eb855da0d5fb165
Andrea Palmatè <info@papersoft.it>
2022-03-23 11:23:11 +0100

    Added simple termcap test

--- library/stdlib/main.c
+++ library/stdlib/main.c
@@ -339,8 +339,8 @@ _main()
 	}
 
     /* Set default terminal mode to "amiga" if not set */
-    const char buffer[6] = "amiga";
-    SetVar("TERM", buffer, FALSE, GVF_LOCAL_ONLY);
+    const char buffer[6] = "amiga\0";
+    SetVar("TERM", buffer, -1, 0);
 
     /* The following code will be executed if the program is to keep
        running in the shell or was launched from Workbench. */
