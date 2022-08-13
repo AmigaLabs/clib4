5869159abfbd8973e3cf37ea53bed7f1cae20b96
Andrea Palmatè <info@papersoft.it>
2022-08-13 20:42:04 +0200

    Removed hidden macro and replaced with HIDDEN

--- library/prng/rand48.h
+++ library/prng/rand48.h
@@ -1,5 +1,5 @@
 #include <stdint.h>
 #include <features.h>
 
-hidden uint64_t __rand48_step(unsigned short *xi, unsigned short *lc);
-extern hidden unsigned short __seed48[7];
+HIDDEN uint64_t __rand48_step(unsigned short *xi, unsigned short *lc);
+extern HIDDEN unsigned short __seed48[7];
