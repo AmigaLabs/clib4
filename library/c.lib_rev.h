ee07555f083246bbe197a97c099eba04007c36d9
Andrea Palmate <andrea.palmate@gmail.com>
2023-06-06 16:28:34 +0200

    c.lib_rev fixes

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			1
 #define REVISION		0
-#define SUBREVISION		0
+#define SUBREVISION		4
 
-#define DATE			"16.04.2023"
-#define VERS			"clib.library 1.0.0"
-#define VSTRING			"clib.library 1.0.0 (16.04.2023)\r\n"
-#define VERSTAG			"\0$VER: clib.library 1.0.0 (16.04.2023)"
+#define DATE			"06.06.2023"
+#define VERS			"clib2.library 1.0.4"
+#define VSTRING			"clib2.library 1.0.4 (06.06.2023)\r\n"
+#define VERSTAG			"\0$VER: clib2.library 1.0.0 (06.06.2023)"
 
 #define VERSION_STR		"1"
 #define REVISION_STR	"0"
-#define SUBREVISION_STR	"0"
+#define SUBREVISION_STR	"4"
 
-#define NEW_VERS		"clib.library 1.0.0"
-#define NEW_VSTRING		"clib.library 1.0.0 (16.04.2023)\r\n"
-#define NEW_VERSTAG		"\0$VER: clib.library 1.0.0 (16.04.2023)"
+#define NEW_VERS		"clib2.library 1.0.4"
+#define NEW_VSTRING		"clib2.library 1.0.4 (06.06.2023)\r\n"
+#define NEW_VERSTAG		"\0$VER: clib2.library 1.0.4 (06.06.2023)"
