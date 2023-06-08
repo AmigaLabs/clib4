deb60a68daa16b794c8c80c631a69f3b5fc23625
Andrea Palmatè <info@papersoft.it>
2023-06-08 19:35:23 +0200

    Updated version. Fixed postrm file

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			1
 #define REVISION		0
-#define SUBREVISION		5
+#define SUBREVISION		6
 
-#define DATE			"07.06.2023"
-#define VERS			"clib2.library 1.0.5"
-#define VSTRING			"clib2.library 1.0.5 (07.06.2023)\r\n"
-#define VERSTAG			"\0$VER: clib2.library 1.0.5 (07.06.2023)"
+#define DATE			"08.06.2023"
+#define VERS			"clib2.library 1.0.6"
+#define VSTRING			"clib2.library 1.0.6 (08.06.2023)\r\n"
+#define VERSTAG			"\0$VER: clib2.library 1.0.6 (08.06.2023)"
 
 #define VERSION_STR		"1"
 #define REVISION_STR	"0"
-#define SUBREVISION_STR	"5"
+#define SUBREVISION_STR	"6"
 
-#define NEW_VERS		"clib2.library 1.0.5"
-#define NEW_VSTRING		"clib2.library 1.0.5 (07.06.2023)\r\n"
-#define NEW_VERSTAG		"\0$VER: clib2.library 1.0.5 (07.06.2023)"
+#define NEW_VERS		"clib2.library 1.0.6"
+#define NEW_VSTRING		"clib2.library 1.0.6 (08.06.2023)\r\n"
+#define NEW_VERSTAG		"\0$VER: clib2.library 1.0.6 (08.06.2023)"
