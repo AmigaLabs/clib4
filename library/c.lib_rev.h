decc597f07cbafa15bad59b3169673db70ac77cd
Andrea Palmatè <info@papersoft.it>
2023-06-16 09:33:11 +0200

    Bumped revision

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			1
 #define REVISION		0
-#define SUBREVISION		10
+#define SUBREVISION		11
 
-#define DATE			"11.06.2023"
-#define VERS			"clib2.library 1.0.10"
-#define VSTRING			"clib2.library 1.0.10 (11.06.2023)\r\n"
-#define VERSTAG			"\0$VER: clib2.library 1.0.10 (11.06.2023)"
+#define DATE			"15.06.2023"
+#define VERS			"clib2.library 1.0.11"
+#define VSTRING			"clib2.library 1.0.11 (15.06.2023)\r\n"
+#define VERSTAG			"\0$VER: clib2.library 1.0.11 (15.06.2023)"
 
 #define VERSION_STR		"1"
 #define REVISION_STR	"0"
-#define SUBREVISION_STR	"10"
+#define SUBREVISION_STR	"11"
 
-#define NEW_VERS		"clib2.library 1.0.10"
-#define NEW_VSTRING		"clib2.library 1.0.10 (11.06.2023)\r\n"
-#define NEW_VERSTAG		"\0$VER: clib2.library 1.0.10 (11.06.2023)"
+#define NEW_VERS		"clib2.library 1.0.11"
+#define NEW_VSTRING		"clib2.library 1.0.11 (15.06.2023)\r\n"
+#define NEW_VERSTAG		"\0$VER: clib2.library 1.0.11 (15.06.2023)"
