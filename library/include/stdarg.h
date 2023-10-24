ddabf787df2a52e919158f6e7590bae7a81e3cff
Andrea Palmate <andrea.palmate@gmail.com>
2023-10-24 11:29:30 +0200

    Added va_* functions to stdarg.h file. Fixed GNUMakefile.os4 to correct use -nostdinc parameter

--- library/include/stdarg.h
+++ library/include/stdarg.h
@@ -9,8 +9,12 @@
 
 __BEGIN_DECLS
 
-#undef _STDARG_H
-#include_next "stdarg.h"
+typedef __builtin_va_list va_list;
+
+#define va_start(v,l)   __builtin_va_start(v,l)
+#define va_end(v)       __builtin_va_end(v)
+#define va_arg(v,l)     __builtin_va_arg(v,l)
+#define va_copy(d,s)    __builtin_va_copy(d,s)
 
 #include <sys/amigaos-va.h>
 
