ae2761d0b1c5627a4a8e492e39a42fb0b7f23289
George Sokianos <walkero@gmail.com>
2024-03-16 18:45:47 +0000

    Reverse changes

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,8 +1,8 @@
-#define VERSION 1
-#define REVISION 2
-#define SUBREVISION 2
+#define VERSION			1
+#define REVISION		0
+#define SUBREVISION		0
 
-#define DATE			"16.03.2024"
-#define VERS			"clib4.library 1.2.3"
-#define VSTRING			"clib4.library 1.2.3 (16.03.2024)\r\n"
-#define VERSTAG			"\0$VER: clib4.library 1.2.3 (16.03.2024)"
+#define DATE			"27.09.2023"
+#define VERS			"clib4.library 1.0.0"
+#define VSTRING			"clib4.library 1.0.0 (27.09.2023)\r\n"
+#define VERSTAG			"\0$VER: clib4.library 1.0.0 (27.09.2023)"
