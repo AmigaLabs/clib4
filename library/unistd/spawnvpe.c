a929daf779f680a6405aa24cc701ecd632eabb29
Andrea Palmatè <info@papersoft.it>
2024-07-03 10:50:09 +0200

    Change spawnvpe to use CreateNewProc instead of SystemTags

--- library/unistd/spawnvpe.c
+++ library/unistd/spawnvpe.c
@@ -16,6 +16,20 @@
 
 #include "children.h"
 
+struct EntryData {
+    uint8 signal;
+    struct Task *mainTask;
+};
+
+STATIC void amiga_EntryCode(int32 entry_data) {
+    // NOTES:
+    // We need to make sure, that the spawned process exists, before the parent can continue.
+    // See the notes for : uv__spawn_and_init_child_posix_spawn
+    struct EntryData *ed = (struct EntryData *) entry_data;
+    if(ed)
+        Signal(ed->mainTask, 1 << ed->signal);
+}
+
 STATIC BOOL
 string_needs_quoting(const char *string, size_t len) {
     BOOL result = FALSE;
@@ -118,21 +132,20 @@ get_arg_string_length(char *const argv[]) {
     return (result);
 }
 
-int
-spawnvpe(const char *file, const char **argv, char **deltaenv, const char *dir, int fhin, int fhout, int fherr) {
+int spawnvpe(const char *file, const char **argv, char **deltaenv, const char *dir, int fhin, int fhout, int fherr) {
     int ret = -1;
+    struct name_translation_info nti_name;
+    const char *name = file;
+    struct name_translation_info nti_cwd;
+    BPTR iofh[3] = {BZERO, BZERO, BZERO};
+    int closefh[3] = {FALSE, FALSE, FALSE};
+    BPTR fh;
+    int err;
+    BPTR progdirLock = 0;
+    struct Task *me = FindTask(NULL);
     char *arg_string = NULL;
     size_t arg_string_len = 0;
     size_t parameter_string_len = 0;
-    struct fd *fd;
-    D(("spawnvpe = fhin = %ld - fhout = %ld - fherr = %ld\n", fhin, fhout, fherr));
-    errno = 0;
-
-    parameter_string_len = get_arg_string_length((char *const *) argv);
-    if (parameter_string_len > _POSIX_ARG_MAX) {
-        __set_errno(E2BIG);
-        return ret;
-    }
 
     arg_string = malloc(parameter_string_len + 1);
     if (arg_string == NULL) {
@@ -140,29 +153,29 @@ spawnvpe(const char *file, const char **argv, char **deltaenv, const char *dir, 
         return ret;
     }
 
-    if (fhin >= 0) {
-        fd = __get_file_descriptor(fhin);
-        if (fd == NULL) {
-            __set_errno(EBADF);
-            return ret;
-        }
+    parameter_string_len = get_arg_string_length((char *const *) argv);
+    if (parameter_string_len > _POSIX_ARG_MAX) {
+        __set_errno(E2BIG);
+        return ret;
     }
 
-    if (fhout >= 0) {
-        fd = __get_file_descriptor(fhout);
-        if (fd == NULL) {
-            __set_errno(EBADF);
-            return ret;
-        }
+    DebugPrintF("[uv__do_create_new_process_amiga :] New process \'%s\'\n", name);
+
+    int error = __translate_unix_to_amiga_path_name(&name, &nti_name);
+    if (error) {
+        printf("%s\n", strerror(error));
+        return ret;
     }
 
-    if (fherr >= 0) {
-        fd = __get_file_descriptor(fherr);
-        if (fd == NULL) {
-            __set_errno(EBADF);
-            return ret;
-        }
+    BPTR fileLock = Lock(name, SHARED_LOCK);
+    if (fileLock) {
+        progdirLock = ParentDir(fileLock);
+        UnLock(fileLock);
     }
+
+    BPTR seglist = LoadSeg(name);
+    if (!seglist)
+        return ret;
 
     if (parameter_string_len > 0) {
         build_arg_string((char *const *) argv, &arg_string[arg_string_len]);
@@ -175,69 +188,91 @@ spawnvpe(const char *file, const char **argv, char **deltaenv, const char *dir, 
     char finalpath[PATH_MAX] = {0};
     snprintf(finalpath, PATH_MAX - 1, "%s %s", file, arg_string);
 
-    BPTR in = BZERO, out = BZERO, err = BZERO;
-    int in_found = -1, out_found = -1, err_found = -1;
+    Printf("File to execute: [%s]\n", finalpath);
 
     if (fhin >= 0) {
-        in_found = __get_default_file(fhin, &in);
-        D(("1) fhin = %ld - in_found = %ld - in = %p", fhin, in_found, in));
-        if (in_found == OK && fhin == STDIN_FILENO) {
-            ChangeMode(CHANGE_FH, in,  CHANGE_MODE_SHARED);
-            in = DupFileHandle(in);
-            D(("2) fhin = %ld - in_found = %ld - in = %p", fhin, in_found, in));
+        err = __get_default_file(fhin, &fh);
+        if (err) {
+            __set_errno(EBADF);
+            return ret;
         }
-    }
-    if (fhout >= 0) {
-        out_found = __get_default_file(fhout, &out);
-        D(("1) fhout = %ld - out_found = %ld - out = %p", fhout, out_found, out));
-        if (out_found == OK && fhout == STDOUT_FILENO) {
-            ChangeMode(CHANGE_FH, out,  CHANGE_MODE_SHARED);
-            out = DupFileHandle(out);
-            D(("2) fhout = %ld - out_found = %ld - out = %p", fhout, out_found, out));
-        }
-    }
-    if (fherr >= 0) {
-        err_found = __get_default_file(fherr, &err);
-        D(("1) fherr = %ld - err_found = %ld - err = %p", fherr, err_found, err));
-        if (err_found == OK && fherr == STDERR_FILENO) {
-            ChangeMode(CHANGE_FH, err,  CHANGE_MODE_SHARED);
-            err = DupFileHandle(err);
-            D(("2) fherr = %ld - err_found = %ld - err = %p", fherr, err_found, err));
-        }
-    }
-
-    //Printf("finalpath = %s - in_found = %ld - out_found = %ld - err_found = %ld\n", finalpath, in_found, out_found, err_found);
-    ret = SystemTags(finalpath,
-                       SYS_Input, in_found == OK ? in : 0,
-                       SYS_Output, out_found == OK ? out : 0,
-                       SYS_Error, err_found == OK ? err : 0,
-                       SYS_UserShell, TRUE,
-                       SYS_Asynch, TRUE,
-                       NP_Child, TRUE,
-                       //NP_CurrentDir, dir != NULL ? Lock(dir, SHARED_LOCK) : 0,
-                       NP_Name, argv[0],
-                       TAG_DONE);
-
-    if (ret != 0) {
-        Printf("Error executing %s\n", finalpath);
-        /* SystemTags failed. Clean up file handles */
-        if (in_found == 0) Close(in);
-        if (out_found == 0) Close(out);
-        if (err_found == 0) Close(err);
-        errno = __translate_io_error_to_errno(IoErr());
+        iofh[0] = DupFileHandle(fh); //in case this is closed by the parent
+        closefh[0] = TRUE;
     }
     else {
-        /*
-         * If mode is set as P_NOWAIT we can retrieve process id calling IoErr()
-         * just after SystemTags. In this case spawnv will return pid
-         */
-        ret = IoErr();
-        if (insertSpawnedChildren(ret, getgid())) {
-            D(("Children with pid %ld and gid %ld inserted into list\n", ret, getgid()));
-        }
-        else {
-            D(("Cannot insert children with pid %ld and gid %ld into list\n", ret, getgid()));
-        }
+        iofh[0] = Open("NIL:", MODE_OLDFILE);
+        closefh[0] = TRUE;
     }
+
+    if (fhout >= 0) {
+        err = __get_default_file(fhout, &fh);
+        if (err) {
+            __set_errno(EBADF);
+            return ret;
+        }
+        iofh[1] = DupFileHandle(fh); //in case this is closed by the parent
+        closefh[1] = TRUE;
+    }
+    else {
+        iofh[1] = Open("NIL:", MODE_OLDFILE);
+        closefh[1] = TRUE;
+    }
+
+    if (fherr >= 0) {
+        err = __get_default_file(fherr, &fh);
+        if (err) {
+            __set_errno(EBADF);
+            return ret;
+        }
+        iofh[2] = DupFileHandle(fh); //in case this is closed by the parent
+        closefh[2] = TRUE;
+    }
+    else {
+        iofh[2] = Open("NIL:", MODE_OLDFILE);
+        closefh[2] = TRUE;
+    }
+
+    struct EntryData *ed = (struct EntryData *) AllocVecTags(sizeof(struct EntryData), AVT_Type, MEMF_SHARED, AVT_ClearWithValue, 0, TAG_DONE);
+
+    ed->signal = AllocSignal(-1);
+    ed->mainTask = me;
+
+    struct Process *p = CreateNewProcTags(
+            NP_Seglist, seglist,
+            NP_FreeSeglist, TRUE,
+            NP_Cli, TRUE,
+            NP_Child, TRUE,
+            NP_NotifyOnDeathSigTask, me,
+            NP_Input, iofh[0],
+            NP_CloseInput, closefh[0],
+            NP_Output, iofh[1],
+            NP_CloseOutput, closefh[1],
+            NP_Error, iofh[2],
+            NP_CloseError, closefh[2],
+            NP_EntryCode, amiga_EntryCode,
+            NP_EntryData, ed,
+            NP_FinalCode, spawnedProcessExit,
+            NP_Name, argv[0],
+            progdirLock ? NP_ProgramDir : TAG_SKIP, progdirLock,
+            NP_Arguments, arg_string,
+            TAG_DONE
+    );
+
+    if (p == NULL) {
+        FreeVec(ed);
+        for (int i = 0; i < 3; i++) {
+            if (closefh[i])
+                Close(iofh[i]);
+        }
+        return ret;
+    }
+    ret = p->pr_ProcessID;
+
+    // wait for the entry signal from the child :
+    Wait(1 << ed->signal | SIGBREAKF_CTRL_C);
+    FreeSignal(ed->signal);
+    FreeVec(ed);
+
+    // success
     return ret;
 }
