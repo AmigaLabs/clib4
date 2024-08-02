d709cd00802ac5eaacb6a7f78ac4d72d6c401ec8
Andrea Palmatè <info@papersoft.it>
2024-08-02 17:50:08 +0200

    Fixed fenv.h was not working without _GNU_SOURCE enabled

--- library/include/fenv.h
+++ library/include/fenv.h
@@ -17,145 +17,145 @@
 
 #if defined __NO_FPRS__ && !defined _SOFT_FLOAT /* E500 */
 
-/* Define bits representing the exception.  We use the bit positions of
-   the appropriate bits in the SPEFSCR...  */
-enum
-  {
-    FE_INEXACT = 1 << (63 - 42),
-#define FE_INEXACT	FE_INEXACT
-    FE_INVALID = 1 << (63 - 43),
-#define FE_INVALID	FE_INVALID
-    FE_DIVBYZERO = 1 << (63 - 44),
-#define FE_DIVBYZERO	FE_DIVBYZERO
-    FE_UNDERFLOW = 1 << (63 - 45),
-#define FE_UNDERFLOW	FE_UNDERFLOW
-    FE_OVERFLOW = 1 << (63 - 46)
-#define FE_OVERFLOW	FE_OVERFLOW
-  };
+    /* Define bits representing the exception.  We use the bit positions of
+       the appropriate bits in the SPEFSCR...  */
+    enum
+      {
+        FE_INEXACT = 1 << (63 - 42),
+    #define FE_INEXACT	FE_INEXACT
+        FE_INVALID = 1 << (63 - 43),
+    #define FE_INVALID	FE_INVALID
+        FE_DIVBYZERO = 1 << (63 - 44),
+    #define FE_DIVBYZERO	FE_DIVBYZERO
+        FE_UNDERFLOW = 1 << (63 - 45),
+    #define FE_UNDERFLOW	FE_UNDERFLOW
+        FE_OVERFLOW = 1 << (63 - 46)
+    #define FE_OVERFLOW	FE_OVERFLOW
+      };
 
-#define FE_ALL_EXCEPT \
-	(FE_INEXACT | FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW | FE_INVALID)
+    #define FE_ALL_EXCEPT \
+        (FE_INEXACT | FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW | FE_INVALID)
 
-#define	FE_ALL_INVALID	FE_INVALID
+    #define	FE_ALL_INVALID	FE_INVALID
 
-/* The E500 support all of the four defined rounding modes.  We use
-   the bit pattern in the SPEFSCR as the values for the appropriate
-   macros.  */
-enum
-  {
-    FE_TONEAREST = 0,
-#define FE_TONEAREST	FE_TONEAREST
-    FE_TOWARDZERO = 1,
-#define FE_TOWARDZERO	FE_TOWARDZERO
-    FE_UPWARD = 2,
-#define FE_UPWARD	FE_UPWARD
-    FE_DOWNWARD = 3
-#define FE_DOWNWARD	FE_DOWNWARD
-  };
+    /* The E500 support all of the four defined rounding modes.  We use
+       the bit pattern in the SPEFSCR as the values for the appropriate
+       macros.  */
+    enum
+      {
+        FE_TONEAREST = 0,
+    #define FE_TONEAREST	FE_TONEAREST
+        FE_TOWARDZERO = 1,
+    #define FE_TOWARDZERO	FE_TOWARDZERO
+        FE_UPWARD = 2,
+    #define FE_UPWARD	FE_UPWARD
+        FE_DOWNWARD = 3
+    #define FE_DOWNWARD	FE_DOWNWARD
+      };
 
-/* Type representing exception flags.  */
-typedef unsigned int fexcept_t;
-typedef uint32_t fenv_t;
+    /* Type representing exception flags.  */
+    typedef unsigned int fexcept_t;
+    typedef uint32_t fenv_t;
 #else
-/* Define bits representing the exception.  We use the bit positions of
-   the appropriate bits in the FPSCR...  */
-enum
-{
-    FE_INEXACT = 1 << (31 - 6),
-#define FE_INEXACT	FE_INEXACT
-    FE_DIVBYZERO = 1 << (31 - 5),
-#define FE_DIVBYZERO	FE_DIVBYZERO
-    FE_UNDERFLOW = 1 << (31 - 4),
-#define FE_UNDERFLOW	FE_UNDERFLOW
-    FE_OVERFLOW = 1 << (31 - 3),
-#define FE_OVERFLOW	FE_OVERFLOW
+    /* Define bits representing the exception.  We use the bit positions of
+       the appropriate bits in the FPSCR...  */
+    enum
+    {
+        FE_INEXACT = 1 << (31 - 6),
+    #define FE_INEXACT	FE_INEXACT
+        FE_DIVBYZERO = 1 << (31 - 5),
+    #define FE_DIVBYZERO	FE_DIVBYZERO
+        FE_UNDERFLOW = 1 << (31 - 4),
+    #define FE_UNDERFLOW	FE_UNDERFLOW
+        FE_OVERFLOW = 1 << (31 - 3),
+    #define FE_OVERFLOW	FE_OVERFLOW
 
-    /* ... except for FE_INVALID, for which we use bit 31. FE_INVALID
-       actually corresponds to bits 7 through 12 and 21 through 23
-       in the FPSCR, but we can't use that because the current draft
-       says that it must be a power of 2.  Instead we use bit 2 which
-       is the summary bit for all the FE_INVALID exceptions, which
-       kind of makes sense.  */
-    FE_INVALID = 1 << (31 - 2),
-#define FE_INVALID	FE_INVALID
+        /* ... except for FE_INVALID, for which we use bit 31. FE_INVALID
+           actually corresponds to bits 7 through 12 and 21 through 23
+           in the FPSCR, but we can't use that because the current draft
+           says that it must be a power of 2.  Instead we use bit 2 which
+           is the summary bit for all the FE_INVALID exceptions, which
+           kind of makes sense.  */
+        FE_INVALID = 1 << (31 - 2),
+    #define FE_INVALID	FE_INVALID
 
-#ifdef __USE_GNU
-    /* Breakdown of the FE_INVALID bits. Setting FE_INVALID on an
-       input to a routine is equivalent to setting all of these bits;
-       FE_INVALID will be set on output from a routine iff one of
-       these bits is set.  Note, though, that you can't disable or
-       enable these exceptions individually.  */
+    #ifdef __USE_GNU
+        /* Breakdown of the FE_INVALID bits. Setting FE_INVALID on an
+           input to a routine is equivalent to setting all of these bits;
+           FE_INVALID will be set on output from a routine iff one of
+           these bits is set.  Note, though, that you can't disable or
+           enable these exceptions individually.  */
 
-    /* Operation with SNaN. */
-    FE_INVALID_SNAN = 1 << (31 - 7),
-# define FE_INVALID_SNAN	FE_INVALID_SNAN
+        /* Operation with SNaN. */
+            FE_INVALID_SNAN = 1 << (31 - 7),
+        # define FE_INVALID_SNAN	FE_INVALID_SNAN
 
-    /* Inf - Inf */
-    FE_INVALID_ISI = 1 << (31 - 8),
-# define FE_INVALID_ISI		FE_INVALID_ISI
+            /* Inf - Inf */
+            FE_INVALID_ISI = 1 << (31 - 8),
+        # define FE_INVALID_ISI		FE_INVALID_ISI
 
-    /* Inf / Inf */
-    FE_INVALID_IDI = 1 << (31 - 9),
-# define FE_INVALID_IDI		FE_INVALID_IDI
+            /* Inf / Inf */
+            FE_INVALID_IDI = 1 << (31 - 9),
+        # define FE_INVALID_IDI		FE_INVALID_IDI
 
-    /* 0 / 0 */
-    FE_INVALID_ZDZ = 1 << (31 - 10),
-# define FE_INVALID_ZDZ		FE_INVALID_ZDZ
+            /* 0 / 0 */
+            FE_INVALID_ZDZ = 1 << (31 - 10),
+        # define FE_INVALID_ZDZ		FE_INVALID_ZDZ
 
-    /* Inf * 0 */
-    FE_INVALID_IMZ = 1 << (31 - 11),
-# define FE_INVALID_IMZ		FE_INVALID_IMZ
+            /* Inf * 0 */
+            FE_INVALID_IMZ = 1 << (31 - 11),
+        # define FE_INVALID_IMZ		FE_INVALID_IMZ
 
-    /* Comparison with NaN or SNaN.  */
-    FE_INVALID_COMPARE = 1 << (31 - 12),
-# define FE_INVALID_COMPARE	FE_INVALID_COMPARE
+            /* Comparison with NaN or SNaN.  */
+            FE_INVALID_COMPARE = 1 << (31 - 12),
+        # define FE_INVALID_COMPARE	FE_INVALID_COMPARE
 
-    /* Invalid operation flag for software (not set by hardware).  */
-    /* Note that some chips don't have this implemented, presumably
-       because no-one expected anyone to write software for them %-).  */
-    FE_INVALID_SOFTWARE = 1 << (31 - 21),
-# define FE_INVALID_SOFTWARE	FE_INVALID_SOFTWARE
+            /* Invalid operation flag for software (not set by hardware).  */
+            /* Note that some chips don't have this implemented, presumably
+               because no-one expected anyone to write software for them %-).  */
+            FE_INVALID_SOFTWARE = 1 << (31 - 21),
+        # define FE_INVALID_SOFTWARE	FE_INVALID_SOFTWARE
 
-    /* Square root of negative number (including -Inf).  */
-    /* Note that some chips don't have this implemented.  */
-    FE_INVALID_SQRT = 1 << (31 - 22),
-# define FE_INVALID_SQRT	FE_INVALID_SQRT
+            /* Square root of negative number (including -Inf).  */
+            /* Note that some chips don't have this implemented.  */
+            FE_INVALID_SQRT = 1 << (31 - 22),
+        # define FE_INVALID_SQRT	FE_INVALID_SQRT
 
-    /* Conversion-to-integer of a NaN or a number too large or too small.  */
-    FE_INVALID_INTEGER_CONVERSION = 1 << (31 - 23)
-# define FE_INVALID_INTEGER_CONVERSION	FE_INVALID_INTEGER_CONVERSION
+            /* Conversion-to-integer of a NaN or a number too large or too small.  */
+            FE_INVALID_INTEGER_CONVERSION = 1 << (31 - 23)
+        # define FE_INVALID_INTEGER_CONVERSION	FE_INVALID_INTEGER_CONVERSION
 
-# define FE_ALL_INVALID \
-        (FE_INVALID_SNAN | FE_INVALID_ISI | FE_INVALID_IDI | FE_INVALID_ZDZ \
-	 | FE_INVALID_IMZ | FE_INVALID_COMPARE | FE_INVALID_SOFTWARE \
-	 | FE_INVALID_SQRT | FE_INVALID_INTEGER_CONVERSION)
-#endif
-};
+        # define FE_ALL_INVALID \
+                (FE_INVALID_SNAN | FE_INVALID_ISI | FE_INVALID_IDI | FE_INVALID_ZDZ \
+             | FE_INVALID_IMZ | FE_INVALID_COMPARE | FE_INVALID_SOFTWARE \
+             | FE_INVALID_SQRT | FE_INVALID_INTEGER_CONVERSION)
+    #endif /* !__USE_GNU */
+    };
 
-#define FE_ALL_EXCEPT \
-	(FE_INEXACT | FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW | FE_INVALID)
+    #define FE_ALL_EXCEPT \
+        (FE_INEXACT | FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW | FE_INVALID)
 
-/* PowerPC chips support all of the four defined rounding modes.  We
-   use the bit pattern in the FPSCR as the values for the
-   appropriate macros.  */
-enum
-{
-    FE_TONEAREST =  0,
-#define FE_TONEAREST	FE_TONEAREST
-    FE_TOWARDZERO = 1,
-#define FE_TOWARDZERO	FE_TOWARDZERO
-    FE_UPWARD =     2,
-#define FE_UPWARD	    FE_UPWARD
-    FE_DOWNWARD =   3
-#define FE_DOWNWARD	    FE_DOWNWARD
-};
+    /* PowerPC chips support all of the four defined rounding modes.  We
+       use the bit pattern in the FPSCR as the values for the
+       appropriate macros.  */
+    enum
+    {
+        FE_TONEAREST =  0,
+    #define FE_TONEAREST	FE_TONEAREST
+        FE_TOWARDZERO = 1,
+    #define FE_TOWARDZERO	FE_TOWARDZERO
+        FE_UPWARD =     2,
+    #define FE_UPWARD	    FE_UPWARD
+        FE_DOWNWARD =   3
+    #define FE_DOWNWARD	    FE_DOWNWARD
+    };
 
-/* Type representing exception flags.  */
-typedef unsigned int fexcept_t;
+    /* Type representing exception flags.  */
+    typedef unsigned int fexcept_t;
 
-/* Type representing floating-point environment.  We leave it as 'double'
-   for efficiency reasons (rather than writing it to a 32-bit integer). */
-typedef uint32_t fenv_t;
+    /* Type representing floating-point environment.  We leave it as 'double'
+       for efficiency reasons (rather than writing it to a 32-bit integer). */
+    typedef uint32_t fenv_t;
 #endif
 
 #define _ROUND_MASK (FE_TONEAREST | FE_DOWNWARD | FE_UPWARD | FE_TOWARDZERO)
@@ -206,8 +206,10 @@ feclearexcept(int __excepts)
 {
     union __fpscr __r;
 
+#ifdef __USE_GNU
     if (__excepts & FE_INVALID)
         __excepts |= FE_ALL_INVALID;
+#endif
     __mffs(&__r);
     __r.__bits.__reg &= ~__excepts;
     __mtfsf(__r);
@@ -229,8 +231,10 @@ fesetexceptflag(const fexcept_t *__flagp, int __excepts)
 {
     union __fpscr __r;
 
+#ifdef __USE_GNU
     if (__excepts & FE_INVALID)
         __excepts |= FE_ALL_INVALID;
+#endif
     __mffs(&__r);
     __r.__bits.__reg &= ~__excepts;
     __r.__bits.__reg |= *__flagp & __excepts;
@@ -246,8 +250,10 @@ feraiseexcept(int __excepts)
 {
     union __fpscr __r;
 
+#ifdef __USE_GNU
     if (__excepts & FE_INVALID)
         __excepts |= FE_INVALID_SOFTWARE;
+#endif
     __mffs(&__r);
     __r.__bits.__reg |= __excepts;
     __mtfsf(__r);
