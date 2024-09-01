51412e0a8ff79f536fc23efbb649464725e9cd2c
Alfkil Thorbjørn Wennermark <alfkil@gmail.com>
2024-09-01 14:37:24 +0200

    Revert initializer to 0 instead of EOF (stdio/fwrite.c). Fixes problem with c++ output and empty string objects.

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -2,7 +2,7 @@
 #define REVISION		0
 #define SUBREVISION		0
 
-#define DATE			"31.08.2024"
+#define DATE			"01.09.2024"
 #define VERS			"clib4.library 1.0.0"
-#define VSTRING			"clib4.library 1.0.0 (31.08.2024)\r\n"
-#define VERSTAG			"\0$VER: clib4.library 1.0.0 (31.08.2024)"
+#define VSTRING			"clib4.library 1.0.0 (01.09.2024)\r\n"
+#define VERSTAG			"\0$VER: clib4.library 1.0.0 (01.09.2024)"
