878cb15e0043d14ba4cac9277fd5017efc563f94
walkero-gr <14014909+walkero-gr@users.noreply.github.com>
2024-12-17 18:10:13 +0000

    Release v1.4.1 changes

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,8 +1,8 @@
 #define VERSION			1
 #define REVISION		4
-#define SUBREVISION		0
+#define SUBREVISION		1
 
-#define DATE			"26.11.2024"
-#define VERS			"clib4.library 1.4.0"
-#define VSTRING			"clib4.library 1.4.0 (26.11.2024)\r\n"
-#define VERSTAG			"\0$VER: clib4.library 1.4.0-1e4d165 (26.11.2024)"
+#define DATE			"17.12.2024"
+#define VERS			"clib4.library 1.4.1"
+#define VSTRING			"clib4.library 1.4.1 (17.12.2024)\r\n"
+#define VERSTAG			"\0$VER: clib4.library 1.4.1-5d728de (17.12.2024)"
