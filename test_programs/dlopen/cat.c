6bf1f58dab39496624ca10a0041f7a8778b04a49
Andrea Palmatè <info@papersoft.it>
2025-02-24 16:34:16 +0100

    Merge Afxgroup branch into development
    * Latest stdio destructor changes was creating a bad problem subtracting index in a loop..
     Some additional tests on flush
    
    * Duplicate IO handles on CreateNewProc
    
    * More WINDOWS to LINUX file ending conversion
    
    * Wrong Printf version requirement printing
    
    * Get DOSIFace in pthread_create from extern _IDOS
    
    * Added %b format type in printf
    
    * Fix on dcngettext was crashing on bindtextdomain and on destructor
    
    * Added Amiga-1251 charset to iconv
    
    * Removed __set_errno from librt since it isn't exported and undefined if you use the library
    
    * Added dlopen example to test shared objects alog rpath and soname
    
    * Removed some quirks from dlopen examples

--- test_programs/dlopen/cat.c
+++ test_programs/dlopen/cat.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
-void print_name(const char* type)
-{
+void print_name(const char *type) {
     FILE *n = fopen("T:test.txt", "w+");
     fprintf(n, "Called\n");
     fprintf(n, "Tama is a %s.\n", type);
     fclose(n);
 
     void *handle;
-    void (*func_print_name)(const char*);
+    void (*func_print_name)(const char *);
 
     handle = dlopen("libdog.so", RTLD_LAZY);
     if (handle != NULL) {
-        *(void**)(&func_print_name) = dlsym(handle, "print_name");
-        func_print_name("Peppe\n");
+        *(void **) (&func_print_name) = dlsym(handle, "print_name");
+        func_print_name("Dog");
         dlclose(handle);
     }
 }
--- test_programs/dlopen/dlopen_sample.c
+++ test_programs/dlopen/dlopen_sample.c
@@ -3,10 +3,9 @@
 #include <string.h>
 #include <dlfcn.h>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     void *handle;
-    void (*func_print_name)(const char*);
+    void (*func_print_name)(const char *);
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s animal_type\n", argv[0]);
@@ -26,22 +25,16 @@ int main(int argc, char** argv)
         fprintf(stderr, "Error: %s\n", dlerror());
         return EXIT_FAILURE;
     }
-#ifdef _CLIB2
-    if (!__global_clib2)
-	printf("__global_clib2 is null\n");
-#endif
 
-    *(void**)(&func_print_name) = dlsym(handle, "print_name");
+    *(void **) (&func_print_name) = dlsym(handle, "print_name");
     if (!func_print_name) {
         /* no such symbol */
         fprintf(stderr, "Error: %s\n", dlerror());
         dlclose(handle);
         return EXIT_FAILURE;
     }
-    printf("argv1 = %s\n", argv[1]);
 
     func_print_name(argv[1]);
-    printf("Closing handle\n");
     dlclose(handle);
 
     return EXIT_SUCCESS;
