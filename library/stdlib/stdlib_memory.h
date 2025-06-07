242b55d925dd4a47ff58f983694a0ea77f2270e8
Andrea Palmatè <info@papersoft.it>
2025-06-07 20:13:55 +0200

    Improved memalign

--- library/stdlib/stdlib_memory.h
+++ library/stdlib/stdlib_memory.h
@@ -30,30 +30,21 @@
 
 #define __static STATIC
 
-struct AVLMemoryNode {
-    /* Variables used to track nodes and free them */
-    struct AVLNode  amn_AvlNode;
-    void           *amn_Address;
-    ULONG           amn_Size;
-};
+// Magic number safe for all endianness
+#define ALIGNED_BLOCK_MAGIC 0xABCD1234DCBA5678ULL
 
-/* At moment to align memory we use AllocVectTags that already align the memory
- * The MemalignEntry structure is used when we call memalign and we have two different
- * ways to free memory. In our case if free is called via memalign
- * we have to use FreeVec otherwise we have to use wof_free
- */
-struct MemalignEntry {
-    struct AVLNode   me_AvlNode;
-    void            *me_Exact;            /* The address returned by AllocVecTags  */
-};
+// Header structure with size optimized for 32/64-bit
+typedef struct {
+#if INTPTR_MAX == INT32_MAX
+	uint32_t padding;  // Maintain 8-byte alignment on 32-bit
+#endif
+	void* original_ptr;
+	uint64_t magic;
+} AlignedHeader;
 
 extern void __memory_lock(struct _clib4 *__clib4);
 extern void __memory_unlock(struct _clib4 *__clib4);
 
 extern void __free_r(struct _clib4 *__clib4, void *ptr);
-
-
-extern int32 MemalignAVLNodeComp(struct AVLNode *avlnode1, struct AVLNode *avlnode2);
-extern int32 MemalignAVLKeyComp(struct AVLNode *avlnode1, AVLKey key2);
 
 #endif /* _STDLIB_MEMORY_H */
