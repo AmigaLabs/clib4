3cc20ed77b56ae6f3cc2964981be0cca7dfb6873
Andrea Palmatè <info@papersoft.it>
2025-06-23 09:05:17 +0200

    Small change on profile reverted (#279)
    * Wrong Printf version requirement printing
    
    * Small profile fixes
    
    * Reverted a small change on profile

--- library/profile/_mcount.c
+++ library/profile/_mcount.c
@@ -32,7 +32,7 @@ __mcount(uint32 frompc, uint32 selfpc) {
      */
     frompc -= p->lowpc;
     selfpc -= p->lowpc;
-    if (frompc > p->text_start + p->textsize)
+    if (frompc > p->textsize)
         goto done;
 
     /* The following test used to be
