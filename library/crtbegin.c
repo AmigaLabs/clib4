282642222ad7a8b1678294410633121e60f2d19a
Andrea Palmatè <info@papersoft.it>
2025-12-08 20:17:17 +0100

    Added .eh_frame to crtbegin which allows now to handle dwarf2 exceptions correctly

--- library/crtbegin.c
+++ library/crtbegin.c
@@ -1,5 +1,6 @@
 /*
 * $Id: crtbegin.c,v 1.3 2022-03-09 21:07:25 clib4devs Exp $
+* Modified to add EH frame support for C++ exceptions
   */
 #undef __USE_INLINE__
 #define __NOLIBBASE__
@@ -48,6 +49,56 @@
 
 static void (*__CTOR_LIST__[1])(void) __attribute__((section(".ctors"))) = { (void *)~0 };
 static void (*__DTOR_LIST__[1])(void) __attribute__((section(".dtors"))) = { (void *)~0 };
+
+/* ===== BEGIN EH FRAME SUPPORT ===== */
+
+/* Object structure for exception handling frame registration */
+struct object {
+    void *pc_begin;
+    void *tbase;
+    void *dbase;
+    union {
+        const void *single;
+        struct dwarf_fde *array;
+    } u;
+    union {
+        struct {
+            unsigned long sorted : 1;
+            unsigned long from_array : 1;
+            unsigned long mixed_encoding : 1;
+            unsigned long encoding : 8;
+            unsigned long count : 21;
+        } b;
+        size_t i;
+    } s;
+    struct object *next;
+};
+
+/* Mark the beginning of the exception handling frames */
+static const char __EH_FRAME_BEGIN__[] __attribute__((used, section(".eh_frame"), aligned(4))) = { };
+
+/* Weak declarations for the frame registration functions from libgcc */
+extern void __register_frame_info(const void *, struct object *) __attribute__((weak));
+extern void *__deregister_frame_info(const void *) __attribute__((weak));
+
+/* Storage for the object that describes this compilation unit's frames */
+static struct object frame_object;
+
+/* Register exception handling frame information */
+static void
+__register_frame_info_clib4(void) {
+    if (__register_frame_info)
+        __register_frame_info(__EH_FRAME_BEGIN__, &frame_object);
+}
+
+/* Deregister exception handling frame information */
+static void
+__deregister_frame_info_clib4(void) {
+    if (__deregister_frame_info)
+        __deregister_frame_info(__EH_FRAME_BEGIN__);
+}
+
+/* ===== END EH FRAME SUPPORT ===== */
 
 const struct Library *SysBase = NULL;
 const struct ExecIFace *IExec = NULL;
@@ -120,6 +171,9 @@ clib4_start(char *args, const int32 arglen, struct Library *sysbase) {
     r13 = &_SDA_BASE_;
     SysBase = sysbase;
 
+    /* Register exception handling frames EARLY, before any C++ code runs */
+    __register_frame_info_clib4();
+
     iexec = (struct ExecIFace *) ((struct ExecBase *) SysBase)->MainInterface;
     iexec->Obtain();
 
@@ -160,6 +214,10 @@ clib4_start(char *args, const int32 arglen, struct Library *sysbase) {
     else {
         iexec->Alert(AT_Recovery | AG_OpenLib | AO_DOSLib);
     }
+
+    /* Deregister EH frames before cleanup */
+    __deregister_frame_info_clib4();
+
     CloseLibraryInterface(iexec, (struct Interface *) iclib4);
     CloseLibraryInterface(iexec, (struct Interface *) IUtility);
     CloseLibraryInterface(iexec, (struct Interface *) idos);
