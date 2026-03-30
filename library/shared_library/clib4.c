AmigaLabs/clib4

commit 33b3436b40dcc1094e8534271729c881dbbe5dd3

Exterbal (means main or _start) Constructors and Destructors now are loaded using find_external_ctors_dtors in libOpen(). This will fix c++ destructor problem was called after libClose(). There is a flag __call_main_executed that is set to TRUE if exe is called via standard main(). I so destructors and constructors are called normally inside main. This will fix software that is using libprhread

library/shared_library/clib4.c
@@ -284,6 +284,55 @@ static void freeEnvironment(struct _clib4 *__clib4) {
     __clib4->__environment = NULL;
 }
 
+/* Try to find external CTOR/DTOR lists from the executable
+ * This allows automatic handling of C++ destructors for -nostartfiles executables
+ */
+static void
+find_external_ctors_dtors(struct _clib4 *__clib4) {
+    BPTR segment_list = IDOS->GetProcSegList(NULL, GPSLF_RUN | GPSLF_SEG);
+    if (segment_list == BZERO) {
+        D(bug("find_external_ctors_dtors: GetProcSegList returned ZERO\n"));
+        return;
+    }
+
+    Elf32_Handle hSelf = NULL;
+    int ret = IDOS->GetSegListInfoTags(segment_list, GSLI_ElfHandle, &hSelf, TAG_DONE);
+    if (ret != 1 || hSelf == NULL) {
+        D(bug("find_external_ctors_dtors: Could not get ELF handle\n"));
+        return;
+    }
+
+    /* Try to find __DTOR_LIST__ symbol in the executable using SymbolQuery */
+    struct Elf32_SymbolQuery query;
+    
+    query.Flags = ELF32_SQ_BYNAME;
+    query.Name = "__DTOR_LIST__";
+    query.NameLength = 0;
+    query.Value = 0;
+    query.Found = FALSE;
+    
+    ULONG found = __IElf->SymbolQuery(hSelf, 1, &query);
+    
+    if (found > 0 && query.Found) {
+        __clib4->__external_dtors = (void (**)(void))query.Value;
+        __clib4->__external_dtors_called = FALSE;
+        D(bug("find_external_ctors_dtors: Found __DTOR_LIST__ at 0x%08lx\n", query.Value));
+    } else {
+        D(bug("find_external_ctors_dtors: __DTOR_LIST__ symbol not found\n"));
+        __clib4->__external_dtors = NULL;
+    }
+}
+
+/* Legacy function kept for compatibility but now does nothing
+ * Destructors are automatically discovered in libOpen()
+ */
+void
+__call_external_dtors(void (**__DTOR_LIST__)(void)) {
+    (void)__DTOR_LIST__;
+    /* This function is now a no-op since we auto-discover and call dtors in libClose() */
+    D(bug("__call_external_dtors: Called but ignored (auto-discovery enabled)\n"));
+}
+
 static void closeLibraries() {
     if (TimeReq != NULL) {
         closeTimer(TimeReq);
@@ -513,6 +562,12 @@ struct Clib4Library *libOpen(struct LibraryManagerInterface *Self, uint32 versio
 
             ITimer->GetSysTime((struct TimeVal *) &__clib4->clock);
 
+            /* Try to find external CTOR/DTOR lists from the executable
+             * This is needed for -nostartfiles executables with C++ code
+             */
+            SHOWMSG("Looking for external destructors");
+            find_external_ctors_dtors(__clib4);
+
             /* At this point exe is fully initialized */
             __clib4->__fully_initialized = TRUE;
             SHOWMSG("Library initialized");
@@ -583,6 +638,30 @@ BPTR libClose(struct LibraryManagerInterface *Self) {
         void *item;
 
         struct _clib4 * __clib4 = (struct _clib4 *) me->pr_UID;
+        
+        struct Task *t = IExec->FindTask(NULL);
+        D(("[__getclib4 :] ln_Type == %ld, pr_UID == %ld\n", t->tc_Node.ln_Type, ((struct Process *)t)->pr_UID));
+
+        /* Call external destructors only for -nostartfiles executables
+         * Detection: call_main() sets __call_main_executed to TRUE for normal executables
+         * For -nostartfiles executables, this flag remains FALSE and we need to call destructors here
+         */
+        if (!__clib4->__call_main_executed && __clib4->__external_dtors != NULL && !__clib4->__external_dtors_called) {
+            SHOWMSG("Calling external dtors (auto-discovered from -nostartfiles exe)");
+            _end_ctors(__clib4->__external_dtors);
+            __clib4->__external_dtors_called = TRUE;
+            SHOWMSG("Done. All external destructors called");
+            
+            /* Also call clib4 internal destructors for -nostartfiles exe
+             * For normal executables, these are already called in call_main()
+             */
+            SHOWMSG("Calling clib4 dtors for -nostartfiles exe");
+            _end_ctors(__DTOR_LIST__);
+            SHOWMSG("Done. All clib4 destructors called");
+        }
+        /* else: Normal executable using library_start() - destructors already called in call_main() */
+
+        /* Now safe to restore task priority and deallocate resources */
         /* Restore the task priority. */
         IExec->SetTaskPri((struct Task *) me, res->oldPriority);
 
@@ -602,13 +681,6 @@ BPTR libClose(struct LibraryManagerInterface *Self) {
             SHOWMSG("Closing randfd[1]");
             close(__clib4->randfd[1]);
         }
-        
-        struct Task *t = IExec->FindTask(NULL);
-        D(("[__getclib4 :] ln_Type == %ld, pr_UID == %ld\n", t->tc_Node.ln_Type, ((struct Process *)t)->pr_UID));
-
-        SHOWMSG("Calling clib4 dtors");
-        _end_ctors(__DTOR_LIST__);
-        SHOWMSG("Done. All destructors called");
 
         SHOWMSG("Calling reent_exit on _clib4");
         reent_exit(__clib4);

GNUmakefile.os4
@@ -73,7 +73,7 @@ WARNINGS := \
 	-Wundef -Wmissing-declarations -Wunused -Wwrite-strings -Wno-unused-value -Wno-comment -Wno-missing-braces \
 	-Wno-deprecated-declarations -Wno-unused-variable -Wno-parentheses -Wno-missing-prototypes \
 	-Wstrict-aliasing -Wno-shadow -Wno-discarded-qualifiers -Wno-unused-function -Wno-unused-parameter -Wno-strict-aliasing \
-    -Wno-type-limits -Wno-cast-function-type -Wno-frame-address -Wno-error=unused-but-set-variable -Wno-stringop-overflow -Werror \
+    -Wno-type-limits -Wno-cast-function-type -Wno-frame-address -Wno-error=unused-but-set-variable -Wno-stringop-overflow -Wno-prio-ctor-dtor -Werror \
      # -Wbad-function-cast -Wconversion -Wformat
 
 PIC		   := -fPIC -DPIC
@@ -231,8 +231,6 @@ ALL_TARGETS = \
 	$(OUTPUT_LIB)/crtend.o \
 	$(OUTPUT_LIB)/shcrtbegin.o \
 	$(OUTPUT_LIB)/shcrtend.o \
-	$(OUT_SHARED_LIB)/shared_library/clib4.o \
-	$(OUT_SHARED_LIB)/shared_library/math.o \
 	$(MAIN_LIB)
 
 ifdef SPE
@@ -287,7 +285,7 @@ version:
 
 # Shared clib4.library rules
 
-clib4.library.debug: $(OBJ_C_LIB)
+clib4.library.debug: $(OUT_SHARED_LIB)/shared_library/clib4.o $(OUT_SHARED_LIB)/shared_library/math.o $(OBJ_C_LIB)
 	$(VERBOSE)$(CC) -o $(BUILD_DIR)/$@ $(CFLAGS) $(BUILD_DIR)/shared/shared_library/clib4.o $(BUILD_DIR)/shared/shared_library/math.o $(OBJ_C_LIB) $(OUTPUT_LIB)/crtend.o $(DLIBS) -Wl,-export-dynamic -Wl,--cref,-M,-Map=$(BUILD_DIR)/$@.map
 
 clib4.library: clib4.library.debug

library/c.lib_rev.h
@@ -2,7 +2,7 @@
 #define REVISION		1
 #define SUBREVISION		0
 
-#define DATE			"29.01.2026"
+#define DATE			"07.02.2026"
 #define VERS			"clib4.library 2.1"
-#define VSTRING			"clib4.library 2.1 (29.01.2026)\r\n"
-#define VERSTAG			"\0$VER: clib4.library 2.1-ab40f98 (29.01.2026)"
+#define VSTRING			"clib4.library 2.1 (07.02.2026)\r\n"
+#define VERSTAG			"\0$VER: clib4.library 2.1-ab40f98 (07.02.2026)"

library/dos.h
@@ -547,6 +547,17 @@ struct _clib4 {
 	/* Custom signals */
 	int8_t _interrupting_alarm_signal_num;
 	uint32_t _interrupting_alarm_signal;
+
+	/* External destructors for -nostartfiles executables 
+	 * Automatically discovered and called in libClose()
+	 */
+	void (**__external_dtors)(void);
+	BOOL __external_dtors_called;
+	
+	/* Flag to track if call_main() was executed (normal executable)
+	 * If FALSE, we're in a -nostartfiles executable and need to call destructors in libClose()
+	 */
+	BOOL __call_main_executed;
 };
 
 #ifndef __getClib4

library/libc_init_global.c
@@ -273,15 +273,20 @@ reent_init(struct _clib4 *__clib4, const BOOL fallback) {
 out:
 
     if (!success) {
-        reent_exit(__clib4);
+    	if (__clib4) {
+    		reent_exit(__clib4);
+    		SHOWMSG("Freeing __clib4 instance pointer");
+    		FreeVec(__clib4);
+    		SHOWMSG("Fallback __clib4 destroyed correctly");
+    	}
     }
 }
 
 void
 reent_exit(struct _clib4 *__clib4) {
     /* Free global clib structure */
-    if (__clib4) {
-    	DECLARE_UTILITYBASE();
+
+	if (__clib4) {
     	if (__clib4->_interrupting_alarm_signal_num >= 0) {
     		FreeSignal(__clib4->_interrupting_alarm_signal_num);
     		__clib4->_interrupting_alarm_signal_num = -1;
@@ -318,10 +323,6 @@ reent_exit(struct _clib4 *__clib4) {
             __clib4->__dl_root_handle = NULL;
             SHOWMSG("Done");
         }
-		ClearMem(__clib4, sizeof(struct _clib4));
-        FreeVec(__clib4);
-        __clib4 = NULL;
-        SHOWMSG("__clib4 destroyed correctly");
     }
 }
 

library/stdlib/free.c
@@ -16,14 +16,17 @@
 
 void
 __free_r(struct _clib4 *__clib4, void *ptr) {
-    if (ptr == NULL)
-        return;
+	if (ptr == NULL || __clib4 == NULL)
+		return;
 
-    __memory_lock(__clib4);
+	if (__clib4->__wmem_allocator == NULL) {
+		return;
+	}
 
+	__memory_lock(__clib4);
     wmem_free(__clib4->__wmem_allocator, ptr);
 
-    __memory_unlock(__clib4);
+	__memory_unlock(__clib4);
 }
 
 void

library/stdlib/main.c
@@ -104,6 +104,10 @@ call_main(
     volatile LONG saved_io_err;
 
     ENTER();
+    
+    /* Mark that call_main() is being executed (normal executable, not -nostartfiles) */
+    __clib4->__call_main_executed = TRUE;
+    
     /* This plants the return buffer for _exit(). */
     if (setjmp(__clib4->__exit_jmp_buf) != 0) {
         D(("Back from longjmp"));

library/stdlib/stdlib_constructor.h
@@ -69,35 +69,38 @@
    destructors. */
 
 #ifdef NEW_CONSTRUCTOR_CALL
-#define STDLIB_CONSTRUCTOR(name)	CONSTRUCTOR(name,	101)
-#define STDLIB_DESTRUCTOR(name)		DESTRUCTOR(name,	101)
 
-#define STK_CONSTRUCTOR(name)		CONSTRUCTOR(name,	102)
-#define STK_DESTRUCTOR(name)		DESTRUCTOR(name,	102)
+#define START_CONSTRUCTOR 100
 
-#define STDIO_CONSTRUCTOR(name)		CONSTRUCTOR(name,	103)
-#define STDIO_DESTRUCTOR(name)		DESTRUCTOR(name,	103)
+#define STDLIB_CONSTRUCTOR(name)	CONSTRUCTOR(name,	START_CONSTRUCTOR)
+#define STDLIB_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR)
 
-#define FILE_CONSTRUCTOR(name)		CONSTRUCTOR(name,	104)
-#define FILE_DESTRUCTOR(name)		DESTRUCTOR(name,	104)
+#define STK_CONSTRUCTOR(name)		CONSTRUCTOR(name,	START_CONSTRUCTOR + 1)
+#define STK_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR + 1)
 
-#define MATH_CONSTRUCTOR(name)		CONSTRUCTOR(name,	105)
-#define MATH_DESTRUCTOR(name)		DESTRUCTOR(name,	105)
+#define STDIO_CONSTRUCTOR(name)		CONSTRUCTOR(name,	START_CONSTRUCTOR + 2)
+#define STDIO_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR + 2)
 
-#define SOCKET_CONSTRUCTOR(name)	CONSTRUCTOR(name,	106)
-#define SOCKET_DESTRUCTOR(name)		DESTRUCTOR(name,	106)
+#define FILE_CONSTRUCTOR(name)		CONSTRUCTOR(name,	START_CONSTRUCTOR + 3)
+#define FILE_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR + 3)
 
-#define ARG_CONSTRUCTOR(name)		CONSTRUCTOR(name,	107)
-#define ARG_DESTRUCTOR(name)		DESTRUCTOR(name,	107)
+#define MATH_CONSTRUCTOR(name)		CONSTRUCTOR(name,	START_CONSTRUCTOR + 4)
+#define MATH_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR + 4)
 
-#define CLIB_CONSTRUCTOR(name)		CONSTRUCTOR(name,	108)
-#define CLIB_DESTRUCTOR(name)		DESTRUCTOR(name,	108)
+#define SOCKET_CONSTRUCTOR(name)	CONSTRUCTOR(name,	START_CONSTRUCTOR + 5)
+#define SOCKET_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR + 5)
 
-#define PTHREAD_CONSTRUCTOR(name)	NSCONSTRUCTOR(name,	107)
-#define PTHREAD_DESTRUCTOR(name)	NSDESTRUCTOR(name,	107)
+#define ARG_CONSTRUCTOR(name)		CONSTRUCTOR(name,	START_CONSTRUCTOR + 6)
+#define ARG_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR + 6)
 
-#define PROFILE_CONSTRUCTOR(name)	CONSTRUCTOR(name,	109)
-#define PROFILE_DESTRUCTOR(name)	DESTRUCTOR(name,	109)
+#define CLIB_CONSTRUCTOR(name)		CONSTRUCTOR(name,	START_CONSTRUCTOR + 7)
+#define CLIB_DESTRUCTOR(name)		DESTRUCTOR(name,	START_CONSTRUCTOR + 7)
+
+#define PTHREAD_CONSTRUCTOR(name)	NSCONSTRUCTOR(name,	START_CONSTRUCTOR + 8)
+#define PTHREAD_DESTRUCTOR(name)	NSDESTRUCTOR(name,	START_CONSTRUCTOR + 8)
+
+#define PROFILE_CONSTRUCTOR(name)	CONSTRUCTOR(name,	START_CONSTRUCTOR + 9)
+#define PROFILE_DESTRUCTOR(name)	DESTRUCTOR(name,	START_CONSTRUCTOR + 9)
 
 #else
 #define STDLIB_CONSTRUCTOR(name)	CONSTRUCTOR(name,	10)

commit e64671c83e4459bfa14d812ecc7c8f151ca91323

Fixed a memory leak introduced in 33b3436b

library/shared_library/clib4.c
@@ -682,24 +682,24 @@ BPTR libClose(struct LibraryManagerInterface *Self) {
         }
         __clib4->__lib_open_count = 0;
 
-        /* Call external destructors only for -nostartfiles executables
-         * Detection: call_main() sets __call_main_executed to TRUE for normal executables
-         * For -nostartfiles executables, this flag remains FALSE and we need to call destructors here
+        /* Call external destructors only for -nostartfiles executables.
+         * For normal executables, call_main() already calls _end_ctors(__EXT_DTOR_LIST__).
          */
         if (!__clib4->__call_main_executed && __clib4->__external_dtors != NULL && !__clib4->__external_dtors_called) {
             SHOWMSG("Calling external dtors (auto-discovered from -nostartfiles exe)");
             _end_ctors(__clib4->__external_dtors);
             __clib4->__external_dtors_called = TRUE;
             SHOWMSG("Done. All external destructors called");
-            
-            /* Also call clib4 internal destructors for -nostartfiles exe
-             * For normal executables, these are already called in call_main()
-             */
-            SHOWMSG("Calling clib4 dtors for -nostartfiles exe");
-            _end_ctors(__DTOR_LIST__);
-            SHOWMSG("Done. All clib4 destructors called");
         }
-        /* else: Normal executable using library_start() - destructors already called in call_main() */
+
+        /* Always call clib4 internal destructors (__DTOR_LIST__).
+         * These include stdlib_memory_exit (frees wmem allocator),
+         * stdio_exit, __pthread_exit, etc.
+         * call_main() only handles __EXT_DTOR_LIST__ (the exe's own dtors),
+         * not the library's internal __DTOR_LIST__. */
+        SHOWMSG("Calling clib4 internal dtors");
+        _end_ctors(__DTOR_LIST__);
+        SHOWMSG("Done. All clib4 destructors called");
 
         /* Now safe to restore task priority and deallocate resources */
         /* Restore the task priority. */