d6fb3a07cea2c2e23a5cc6204a66ca077435cef4
Andrea Palmatè <info@papersoft.it>
2026-06-07 18:38:37 +0200

    Update version and revision numbers in c.lib_rev.h

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,8 +1,7 @@
-#define VERSION                 2
-#define REVISION                2
-#define SUBREVISION             0
+#define VERSION			2
+#define REVISION		1
 
-#define DATE                    "07.06.2026"
-#define VERS                    "clib4.library 2.2"
-#define VSTRING                 "clib4.library 2.2 (07.06.2026)\r\n"
-#define VERSTAG                 "\0$VER: clib4.library 2.2-ab40f98 (07.06.2026)"
+#define DATE			"07.06.2026"
+#define VERS			"clib4.library 2.1"
+#define VSTRING			"clib4.library 2.1 (07.06.2026)\r\n"
+#define VERSTAG			"\0$VER: clib4.library 2.1-nightly-c971606 (07.06.2026)"
